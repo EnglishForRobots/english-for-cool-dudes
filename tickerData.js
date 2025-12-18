6b8561e388dd206ebca7fc7c2487ecf42ebc31d2
EnglishForRobots <tonyfrombrighton@gmail.com>
2025-12-18 14:46:13 +0100

    Update tickerData.js

--- tickerData.js
+++ tickerData.js
@@ -2,7 +2,7 @@
 
 // --- 1. DATA SOURCE ---
 const LESSON_STATES = [
-    { text: "🎅🏻 NEW! TAX: The Audit Before Christmas 😎", href: "/santatax/" },
+    { text: "🎅🏻 NEW! TAX: The Audit Before Christmas 🎄", href: "/santatax/" },
     { text: "🍷 NEW! ADVANCED: The Science and Culture of Mulled Wine 😎", href: "/mulledwine/" },
     { text: "🎁 NEW! INTERMEDIATE: The History of Advent Calendars 😎", href: "/adventcalendarint/" },
     { text: "🎅🏻 NEW! BEGINNER: The Advent Calendar 😎", href: "/adventcalendar/" },
