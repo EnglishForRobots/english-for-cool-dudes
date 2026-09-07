49fdf51eec780beef9ce3c82e0515be9543f2461
EnglishForRobots <tonyfrombrighton@gmail.com>
2026-09-07 12:45:44 +0200

    Update level-page.js

--- level-page.js
+++ level-page.js
@@ -219,6 +219,7 @@
    'weeklydrop019':  'weekly-drop-issue-019',
       'weeklydropoil':  'weekly-drop-issue-oil',
      'wigtown':  'wigtown-intermediate',
+     'guardian-london':  'guardian-london-advanced',
   };
 
   async function bootAuthStrip(eyebrowText) {
